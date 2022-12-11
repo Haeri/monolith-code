beec5c43fe7a3234198616fce55ce1475e7a6d78
Haeri <luka.l@protonmail.ch>
2022-12-11 15:24:53 +0100

    update

--- src/main.js
+++ src/main.js
@@ -5,46 +5,46 @@ import { appWindow } from "@tauri-apps/api/window";
 
 
 window.INFO_LEVEL = Object.freeze({
-    user: 0,
-    info: 1,
-    confirm: 2,
-    warn: 3,
-    error: 4,
-  });
+  user: 0,
+  info: 1,
+  confirm: 2,
+  warn: 3,
+  error: 4,
+});
 
 
 const initialSettings = {
-    "appInfo": {
-        "name": "monolith code",
-        "version": "2.2.4",
-        "os": "win32"
-    },
-    "filePathsToOpen": [],
-    "editorConfig": {
-        "theme": "ace/theme/monokai",
-        "media_div_percent": "100%",
-        "console_div_percent": 50,
-        "font_size": 14,
-        "line_wrapping": true,
-        "line_numbers": true
-    },
-    "windowConfig": {
-        "native_frame": false
-    },
-    "localWindowConfig": {
-        "x": 646,
-        "y": 209,
-        "width": 636,
-        "height": 497,
-        "maximized": false
-    },
-    "languageConfig": {
-        "cpp": {
-            "comp": "g++ -std=c++17 <name>.cpp -o <name>"
-        }
-    },
-    "userPrefPath": "C:\\Users\\Luka\\AppData\\Roaming\\monolith-code\\user-preferences.json",
-    "languageConfigPath": "C:\\Users\\Luka\\AppData\\Roaming\\monolith-code\\lang-settings.json"
+  "appInfo": {
+    "name": "monolith code",
+    "version": "2.2.4",
+    "os": "win32"
+  },
+  "filePathsToOpen": [],
+  "editorConfig": {
+    "theme": "ace/theme/monokai",
+    "media_div_percent": "100%",
+    "console_div_percent": 50,
+    "font_size": 14,
+    "line_wrapping": true,
+    "line_numbers": true
+  },
+  "windowConfig": {
+    "native_frame": false
+  },
+  "localWindowConfig": {
+    "x": 646,
+    "y": 209,
+    "width": 636,
+    "height": 497,
+    "maximized": false
+  },
+  "languageConfig": {
+    "cpp": {
+      "comp": "g++ -std=c++17 <name>.cpp -o <name>"
+    }
+  },
+  "userPrefPath": "C:\\Users\\Luka\\AppData\\Roaming\\monolith-code\\user-preferences.json",
+  "languageConfigPath": "C:\\Users\\Luka\\AppData\\Roaming\\monolith-code\\lang-settings.json"
 };
 
 window.api = {
@@ -53,22 +53,22 @@ window.api = {
   getInitialSettings: () => {
     console.log("called", "getInitialSettings");
     return initialSettings;
-    },
+  },
 
   // Window API
   minimize: () => {
     console.log("called", "minimize");
     appWindow.minimize();
-},
+  },
   maximize: () => {
     console.log("called", "maximize");
     appWindow.toggleMaximize();
-},
+  },
   unmaximize: () => console.log("called", "unmaximize"),
   toggleMaxUnmax: () => {
     console.log("called", "toggleMaxUnmax");
     appWindow.toggleMaximize();
-},
+  },
   close: () => console.log("called", "close"),
   toggleAlwaysOnTop: () => console.log("called", "toggleAlwaysOnTop"),
   newWindow: (filePaths) => console.log("called", "newWindow"),
@@ -76,9 +76,9 @@ window.api = {
 
   // Features API
   showOpenDialog: () => {
-    console.log("called", "showOpenDialog"); 
+    console.log("called", "showOpenDialog");
     return dialog.open({ title: "Open a file", multiple: true });
-},
+  },
   showSaveDialog: (options) => console.log("called", "showSaveDialog"),
 
   storeSetting: (key, value) => console.log("called", "storeSetting"),
@@ -99,5 +99,5 @@ window.api = {
 };
 
 
-  
+
 createApp(App).mount('#app')
